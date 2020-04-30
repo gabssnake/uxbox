44af3111d5ad2d027b297b01e00e18f06a7d368c
Andrey Antukh <niwi@niwi.nz>
2020-04-30 17:16:10 +0200

    :recycle: Refactor html5 history.

---
+++ frontend/src/uxbox/util/browser_history.js
@@ -0,0 +1,45 @@
+/**
+ * intervaltree
+ *
+ * @author Andrey Antukh <niwi@niwi.nz>, 2016
+ * @license MIT License <https://opensource.org/licenses/MIT>
+ */
+
+"use strict";
+
+goog.provide("uxbox.util.browser_history");
+goog.require("goog.history.Html5History");
+
+
+goog.scope(function() {
+  const self = uxbox.util.browser_history;
+  const Html5History = goog.history.Html5History;
+
+  class TokenTransformer {
+    retrieveToken(pathPrefix, location) {
+      return location.pathname.substr(pathPrefix.length) + location.search;
+    }
+
+    createUrl(token, pathPrefix, location) {
+      return pathPrefix + token;
+    }
+  }
+
+  self.create = function() {
+    const instance = new Html5History(null, new TokenTransformer());
+    instance.setUseFragment(true);
+    return instance;
+  };
+
+  self.enable_BANG_ = function(instance) {
+    instance.setEnabled(true);
+  };
+
+  self.disable_BANG_ = function(instance) {
+    instance.setEnabled(false);
+  };
+
+  self.set_token_BANG_ = function(instance, token) {
+    instance.setToken(token);
+  }
+});
